6cda3eea42c6b95f5173da2919a7eb4427e4b108
Luka <fantini706@gmail.com>
2024-07-03 09:53:51 -0300

    Ex12

--- Ex12.js
+++ Ex12.js
@@ -1,14 +1,44 @@
 let tamanho = Number(prompt("Tamanho do array"));
 let array = [];
 for (let i = 0; i < tamanho; i++){
-    let valor = prompt("Numero " + i);
+    let valor = Number(prompt("Numero " + i));
     array.push(valor);
 }
 
-array.sort((a, b) => {
-    if (a > b) return 1;
-    if (a < b) return -1;
-    return 0;
-});
+var bubbleSort = function (array, compare) {
+    var length = array.length;
+    for (var j = 0; j < array.length; j++) {
+      for (var i = 0; i < array.length-j-1; i++) {
+        if (compare(array[i], array[i+1])) {//array[i] > array[i+1]
+            let temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;
+        }
+      }
+    }
+    return array;
+};
+
+function crescente(a, b){
+    if (a > b) return true;
+}
+
+function decrescente(a, b){
+    if (a < b) return true;
+}
+
+function crescenteImpar(a, b){
+    if (a > b && ((a%2 !== 0) && (b%2 !== 0))) return true;
+    if (((a%2 === 0) && (b%2 !== 0))) return true;
+}
+
+function decrescentePar(a, b){
+    if (a < b && ((a%2 === 0) && (b%2 === 0))) return true;
+    if (((a%2 !== 0) && (b%2 === 0))) return true;
+}
 
 console.log(array);
+console.log("Crescente: " + bubbleSort(array, crescente));
+console.log("Decrescente: " + bubbleSort(array, decrescente));
+console.log("Crescente impar: " + bubbleSort(array, crescenteImpar));
+console.log("Decrescente par: " + bubbleSort(array, decrescentePar));
